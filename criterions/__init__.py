f0e8843a1efeeb644deb55d96fd0aeaa40575a17
azxj <zxj13413431649@gmail.com>
2021-12-06 13:34:34 +0800

    feat(criterion): support several criterions
    Supported criterions:
    - CELoss
    - SoftmaxCELoss
    - BCELoss
    - SigmoidBCELoss
    - DiceLoss
    - SigmoidDiceLoss

---
+++ criterions/__init__.py
@@ -0,0 +1,22 @@
+from configs import CFG
+from .bce import BCELoss, SigmoidBCELoss
+from .ce import CELoss, SoftmaxCELoss
+from .dice import DiceLoss, SigmoidDiceLoss
+
+
+def build_criterion():
+    if CFG.CRITERION.NAME == 'ce':
+        criterion = CELoss()
+    elif CFG.CRITERION.NAME == 'softmax+ce':
+        criterion = SoftmaxCELoss()
+    elif CFG.CRITERION.NAME == 'bce':
+        criterion = BCELoss()
+    elif CFG.CRITERION.NAME == 'sigmoid+bce':
+        criterion = SigmoidBCELoss()
+    elif CFG.CRITERION.NAME == 'dice':
+        criterion = DiceLoss()
+    elif CFG.CRITERION.NAME == 'sigmoid+dice':
+        criterion = SigmoidDiceLoss()
+    else:
+        raise NotImplementedError('invalid criterion: {}'.format(CFG.CRITERION.NAME))
+    return criterion
