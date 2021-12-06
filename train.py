6d9629ae489015d1528665a899972b302c9d809f
azxj <zxj13413431649@gmail.com>
2021-12-06 21:02:10 +0800

    fix(train): cancel merging arguments

--- train.py
+++ train.py
@@ -40,9 +40,8 @@ def parse_args():
 
 
 def main():
-    # merge arguments to config
+    # parse command line arguments
     args = parse_args()
-    CFG.merge_from_file(args.config)
 
     # create experiment output path if not exists
     if not os.path.exists(args.path):
