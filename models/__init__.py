69c250d27d12e1279b5764b792aada86ce66ed22
azxj <zxj13413431649@gmail.com>
2022-02-16 23:49:25 +0800

    feat(model): use segmentation-models-pytorch library

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,13 @@
+import segmentation_models_pytorch as smp
+
 from configs import CFG
 from .deeplabv3 import DeepLabV3ResNet18, DeepLabV3ResNet34, DeepLabV3ResNet50, DeepLabV3ResNet101
 
 
 def build_model(num_channels, num_classes):
-    if CFG.MODEL.NAME == 'deeplabv3+resnet18':
-        return DeepLabV3ResNet18(num_channels, num_classes)
-    elif CFG.MODEL.NAME == 'deeplabv3+resnet34':
-        return DeepLabV3ResNet34(num_channels, num_classes)
-    elif CFG.MODEL.NAME == 'deeplabv3+resnet50':
-        return DeepLabV3ResNet50(num_channels, num_classes)
-    elif CFG.MODEL.NAME == 'deeplabv3+resnet101':
-        return DeepLabV3ResNet101(num_channels, num_classes)
+    if CFG.MODEL.NAME == 'deeplabv3':
+        return smp.DeepLabV3(encoder_name=CFG.MODEL.BACKBONE.NAME,
+                             in_channels=num_channels,
+                             classes=num_classes)
     else:
         raise NotImplementedError('invalid model: {}'.format(CFG.MODEL.NAME))
